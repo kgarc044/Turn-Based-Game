b64f6de4a7576e7e70adc1fc0f91762c6fd7e296
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2021-06-10 23:03:19 -0700

    Tutorial Series Part 2 Completed
    Added button components to sprites. Started making animations for the wizard.

---
+++ Turn Based RPG Tutorial/Assets/Scripts/FighterAction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject hero;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject rangePrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+    private GameObject meleeAttack;
+    private GameObject rangeAttack;
+
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+        if (tag == "Hero")
+        {
+            victim = enemy;
+        }
+        if (btn.CompareTo("melee") == 0)
+        {
+            Debug.Log("Melee Attack!");
+        }
+        else if (btn.CompareTo("range") == 0)
+        {
+            Debug.Log("Range Attack!");
+        }
+        else
+        {
+            Debug.Log("Run");
+        }
+    }
+}
